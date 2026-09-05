c31657c7886d975e799c44bc3d500e7e7a17ab54
Sonya Abdullaeva <sophiaabdullaeva2007@icloud.com>
2026-09-05 12:26:39 +0300

    initial commit

---
+++ src/main/java/ru/universitybooking/UniversityBookingApplication.java
@@ -0,0 +1,13 @@
+package ru.universitybooking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UniversityBookingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UniversityBookingApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/universitybooking/UniversityBookingApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.universitybooking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UniversityBookingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
