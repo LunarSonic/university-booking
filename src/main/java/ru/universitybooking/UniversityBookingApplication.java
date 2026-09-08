5cbb4e5af5173a1c48d0e17814a61ea727d55f08
Sonya Abdullaeva <sophiaabdullaeva2007@icloud.com>
2026-09-08 22:55:58 +0300

    clean up BasketItem indexes on TTL expiration

--- src/main/java/ru/universitybooking/UniversityBookingApplication.java
+++ src/main/java/ru/universitybooking/UniversityBookingApplication.java
@@ -2,8 +2,11 @@ package ru.universitybooking;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
+import org.springframework.data.redis.core.RedisKeyValueAdapter.EnableKeyspaceEvents;
 
 @SpringBootApplication
+@EnableRedisRepositories(enableKeyspaceEvents = EnableKeyspaceEvents.ON_STARTUP)
 public class UniversityBookingApplication {
 
     public static void main(String[] args) {
